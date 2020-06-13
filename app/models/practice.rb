b6d1a8db26d042d575447a290691127502b5979c
kota kanazawa <kota0818@gmail.com>
2020-06-13 21:58:30 +0900

    参考書モデルを作成

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -23,6 +23,7 @@ class Practice < ApplicationRecord
     source: :user
   has_many :products
   has_many :questions
+  has_many :reference_books, dependent: :destroy
   belongs_to :category
   acts_as_list scope: :category
   has_one :learning_minute_statistic
